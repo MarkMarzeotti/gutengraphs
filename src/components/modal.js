6b1287a96b7d5cd24073b59ff1d8e8a0980bd2c7
Mark Marzeotti <mark@markmarzeotti.com>
2019-11-04 20:31:57 -0500

    changed datasheet and add row and col functionality

--- src/components/modal.js
+++ src/components/modal.js
@@ -2,11 +2,7 @@ import { Component } from 'react';
 
 const { Button, Modal } = wp.components;
 
-// import SpreadsheetComponent from 'react-spreadsheet-component';
-import Spread from './spread';
-
-// jexcel import
-// import Spreadsheet from './spreadsheet';
+import Spreadsheet from './spreadsheet';
 
 class DataModal extends Component {
 	constructor( props ) {
@@ -14,47 +10,11 @@ class DataModal extends Component {
 
 		this.state = {
 			isOpen: false,
-			cols: 5,
-			rows: 6,
-			chartData: {
-				rows: props.chartData,
-			},
+			chartData: props.chartData,
 		};
 	}
 
 	render() {
-		const config = {
-			rows: 5,
-			columns: 4,
-			hasHeadColumn: false,
-			isHeadColumnString: false,
-			hasHeadRow: false,
-			isHeadRowString: false,
-			canAddRow: true,
-			canAddColumn: true,
-			emptyValueSymbol: '',
-			hasLetterNumberHeads: false,
-		};
-
-		// jexcel options
-		// const data = [
-		// 	[ 'Jazz', 'Honda', '2019-02-12', '', true, '$ 2.000,00', '#777700' ],
-		// 	[ 'Civic', 'Honda', '2018-07-11', '', true, '$ 4.000,01', '#007777' ],
-		// ];
-
-		// const options = {
-		// 	data: data,
-		// 	columns: [
-		// 		{ type: 'text', title: 'Car', width: '120px' },
-		// 		{ type: 'dropdown', title: 'Make', width: '250px', source: [ 'Alfa Romeo', 'Audi', 'Bmw' ] },
-		// 		{ type: 'calendar', title: 'Available', width: '250px' },
-		// 		{ type: 'image', title: 'Photo', width: '120px' },
-		// 		{ type: 'checkbox', title: 'Stock', width: '80px' },
-		// 		{ type: 'numeric', title: 'Price', width: '100px', mask: '$ #.##,00', decimal: ',' },
-		// 		{ type: 'color', width: '100px', render: 'square' },
-		// 	],
-		// };
-
 		return (
 			<div>
 				<Button isDefault onClick={ () => this.setState( { isOpen: true } ) }>Edit Chart Data</Button>
@@ -66,9 +26,7 @@ class DataModal extends Component {
 							<p>Empty rows and columns are cleared on graph update. To create a new row or column, focus the last cell in a row or column and navigate down or to the right respectively.</p>
 						</div>
 						<div className="gutengraphs-datasheet">
-							{/* <SpreadsheetComponent initialData={ this.state.chartData } config={ config } /> */}
-							{ /* <Spreadsheet options={ options } /> */ }
-							<Spread chartData={ this.props.chartData } />
+							<Spreadsheet chartData={ this.props.chartData } />
 						</div>
 						<div className="gutengraphs-datasheet-ui">
 							<Button isDefault isPrimary onClick={ () => {
--- src/components/spreadsheet.js
+++ src/components/spreadsheet.js
@@ -1,27 +1,120 @@
-import { Component } from 'react';
-import { ReactDOM } from 'react-dom';
-import { jexcel } from 'jexcel';
+import { Component, Fragment } from 'react';
+
+const { Button } = wp.components;
+
+import ReactDataSheet from 'react-datasheet';
 
 class Spreadsheet extends Component {
 	constructor( props ) {
 		super( props );
-		this.options = props.options;
+		const grid = [
+			[ { value: 1 }, { value: 3 } ],
+			[ { value: 2 }, { value: 4 } ],
+		];
+		this.state = {
+			grid,
+			cell: {
+				start: { i: 0, j: 0 },
+				end: { i: grid[ 0 ].length - 1, j: grid.length - 1 },
+			},
+		};
 	}
 
-	componentDidMount = function() {
-		this.el = jexcel( ReactDOM.findDOMNode( this ).children[ 0 ], this.options );
+	handleAddRow = below => {
+		let grid = [ ...this.state.grid ];
+
+		const newRow = grid[ 0 ].map( row => {
+			return { value: null };
+		} );
+
+		if ( below && this.state.cell.end.i === this.state.grid.length - 1 ) {
+			grid = [ ...grid, newRow ];
+			// need to recalc cell here so user is allowed to add multiple rows
+			const cell = { ...this.state.cell };
+			cell.end.i = this.state.grid.length;
+			this.setState( { cell } );
+		} else if ( ! below && this.state.cell.start.i === 0 ) {
+			grid = [ newRow, ...grid ];
+		} else if ( below ) {
+			const index = this.state.cell.end.i + 1;
+			grid.splice( index, 0, newRow );
+		} else {
+			const index = this.state.cell.start.i;
+			grid.splice( index, 0, newRow );
+		}
+
+		this.setState( { grid } );
 	}
 
-	addRow = function() {
-		this.el.insertRow();
+	handleAddCol = right => {
+		let grid = [ ...this.state.grid ];
+
+		if ( right && this.state.cell.end.j === this.state.grid[ 0 ].length - 1 ) {
+			grid = grid.map( row => {
+				return [ ...row, { value: null } ];
+			} );
+			// need to recalc cell here so user is allowed to add multiple cols
+			const cell = { ...this.state.cell };
+			cell.end.j = this.state.grid[ 0 ].length;
+			this.setState( { cell } );
+		} else if ( ! right && this.state.cell.start.j === 0 ) {
+			grid = grid.map( row => {
+				return [ { value: null }, ...row ];
+			} );
+		} else if ( right ) {
+			const index = this.state.cell.end.j + 1;
+			grid = grid.map( row => {
+				row.splice( index, 0, { value: null } );
+				return row;
+			} );
+		} else {
+			const index = this.state.cell.start.j;
+			grid = grid.map( row => {
+				row.splice( index, 0, { value: null } );
+				return row;
+			} );
+		}
+
+		this.setState( { grid } );
 	}
+
+	handleLargePaste() {
+
+	}
+
+	// handle larger paste
+	// hook up to graph
 
 	render() {
 		return (
-			<div>
-				<div></div><br />
-				<input type="button" value="Add new row" onClick={ () => this.addRow() }></input>
-			</div>
+			<Fragment>
+				<div className="gutengraphs-datasheet__buttons">
+					<Button isDefault isSmall onClick={ () => this.handleAddRow( 0 ) }>Add Row Above</Button>
+					<Button isDefault isSmall onClick={ () => this.handleAddRow( 1 ) }>Add Row Below</Button>
+					<Button isDefault isSmall onClick={ () => this.handleAddCol( 0 ) }>Add Column Left</Button>
+					<Button isDefault isSmall onClick={ () => this.handleAddCol( 1 ) }>Add Column Right</Button>
+				</div>
+				<ReactDataSheet
+					ref={ this.myRef }
+					data={ this.state.grid }
+					valueRenderer={ ( cell ) => cell.value }
+					onCellsChanged={ ( changes, overflow = null ) => {
+						const grid = this.state.grid.map( row => [ ...row ] );
+						changes.forEach( ( { cell, row, col, value } ) => {
+							grid[ row ][ col ] = { ...grid[ row ][ col ], value };
+						} );
+
+						if ( overflow !== null ) {
+							console.log( overflow );
+						}
+						this.setState( { grid } );
+					} }
+					onSelect={ cell => {
+						this.setState( { cell } );
+						console.log( cell );
+					} }
+				/>
+			</Fragment>
 		);
 	}
 }
